bcd8b7b270d69455ce9e5edc56912ed32988d98c
phattaraporntheprak <theprak16.ep11@gmail.com>
2023-09-03 08:38:31 +0700

    Initial commit

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var value1 = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'HELLO',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 8.0,
+                          ),
+                          child: Text(
+                            'Pattaraporn Theprak ',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        //SizedBox(width: 8.0,),
+                        Image.asset(
+                          'assets/images/hand.png',
+                          width: 40.0,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 55.0,
+                  height: 55.0,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/images/img1.jpeg',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Color.fromARGB(225, 222, 250, 255),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                    child: Text(
+                      'Courses',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: CategoryButton(
+                          text: 'All Topics',
+                          icon: Icons.water_drop,
+                          color: Colors.lightBlue,
+                          isSelected: value1,
+                        )),
+                        SizedBox(
+                          width: 16.0,
+                        ),
+                        Expanded(
+                            child: CategoryButton(
+                          text: 'Popular',
+                          icon: Icons.bolt_outlined,
+                          color: Colors.pink,
+                          isSelected: true,
+                        )),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: CategoryButton(
+                          text: 'Newest',
+                          icon: Icons.star,
+                          color: Colors.orange,
+                          isSelected: true,
+                        )),
+                        SizedBox(
+                          width: 16.0,
+                        ),
+                        Expanded(
+                            child: CategoryButton(
+                          text: 'Advanced',
+                          icon: Icons.bookmark,
+                          color: Colors.green,
+                          isSelected: true,
+                        )),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            value1 = !value1;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('TEST'),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryButton extends StatelessWidget {
+  String text;
+  IconData icon;
+  Color color;
+  bool isSelected;
+
+  CategoryButton({
+    super.key,
+    required this.text,
+    required this.icon,
+    required this.color,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //padding: const EdgeInsets.all(6.0),
+      decoration: BoxDecoration(
+        color: isSelected ? color : Colors.white,
+        borderRadius: BorderRadius.circular(50.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 7,
+            blurRadius: 13,
+            offset: Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: isSelected ? Colors.white : color,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              icon,
+              color: isSelected ? color : Colors.white,
+            ),
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              color: isSelected ? Colors.white : Colors.black87,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
